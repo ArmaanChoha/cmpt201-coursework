8cc176c5193fbb074a7d2f89275336419aa1c47c
Armaan Chohan <armaanchohan129@gmail.com>
2025-11-30 19:38:58 -0800

    lab 11

---
+++ lab11.c
@@ -0,0 +1,146 @@
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define RED "\e[9;31m"
+#define GRN "\e[0;32m"
+#define CRESET "\e[0m"
+
+#define handle_error(msg)                                                      \
+  do {                                                                         \
+    perror(msg);                                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
+
+size_t read_all_bytes(const char *filename, void *buffer, size_t buffer_size) {
+  FILE *file = fopen(filename, "rb");
+  if (!file) {
+    handle_error("Error opening file");
+  }
+
+  fseek(file, 0, SEEK_END);
+  long file_size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  if (file_size > buffer_size) {
+    handle_error("File size is too large");
+  }
+
+  if (fread(buffer, 1, file_size, file) != file_size) {
+    handle_error("Error reading file");
+  }
+
+  fclose(file);
+  return file_size;
+}
+
+void print_file(const char *filename, const char *color) {
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    handle_error("Error opening file");
+  }
+
+  printf("%s", color);
+  char line[256];
+  while (fgets(line, sizeof(line), file)) {
+    printf("%s", line);
+  }
+  fclose(file);
+  printf(CRESET);
+}
+
+int verify(const char *message_path, const char *sign_path, EVP_PKEY *pubkey);
+
+int main() {
+  // File paths
+  const char *message_files[] = {"message1.txt", "message2.txt",
+                                 "message3.txt"};
+  const char *signature_files[] = {"signature1.sig", "signature2.sig",
+                                   "signature3.sig"};
+
+  // TODO: Load the public key using PEM_read_PUBKEY
+  EVP_PKEY *pubkey = NULL;
+  FILE *keyfile = fopen("public_key.pem", "r");
+  if (!keyfile) {
+    handle_error("Error opening public key file");
+  }
+  pubkey = PEM_read_PUBKEY(keyfile, NULL, NULL, NULL);
+  fclose(keyfile);
+  if (!pubkey) {
+    fprintf(stderr, "Error reading public key\n");
+    ERR_print_errors_fp(stderr);
+    exit(EXIT_FAILURE);
+  }
+  // Verify each message
+  for (int i = 0; i < 3; i++) {
+    printf("... Verifying message %d ...\n", i + 1);
+    int result = verify(message_files[i], signature_files[i], pubkey);
+
+    if (result < 0) {
+      printf("Unknown authenticity of message %d\n", i + 1);
+      print_file(message_files[i], CRESET);
+    } else if (result == 0) {
+      printf("Do not trust message %d!\n", i + 1);
+      print_file(message_files[i], RED);
+    } else {
+      printf("Message %d is authentic!\n", i + 1);
+      print_file(message_files[i], GRN);
+    }
+  }
+
+  EVP_PKEY_free(pubkey);
+
+  return 0;
+}
+
+/*
+    Verify that the file `message_path` matches the signature `sign_path`
+    using `pubkey`.
+    Returns:
+         1: Message matches signature
+         0: Signature did not verify successfully
+        -1: Message is does not match signature
+*/
+int verify(const char *message_path, const char *sign_path, EVP_PKEY *pubkey) {
+#define MAX_FILE_SIZE 512
+  unsigned char message[MAX_FILE_SIZE];
+  unsigned char signature[MAX_FILE_SIZE];
+
+  size_t message_len = read_all_bytes(message_path, message, sizeof(message));
+  size_t signature_len =
+      read_all_bytes(sign_path, signature, sizeof(signature));
+  EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+
+  if (!ctx) {
+    fprintf(stderr, "Error: EVP_MD_CTX_new failed\n");
+    return -1;
+  }
+  int ret = -1;
+  if (EVP_DigestVerifyInit(ctx, NULL, EVP_sha256(), NULL, pubkey) != 1) {
+    fprintf(stderr, "Error: EVP_DigestVerifyInit failed\n");
+    ERR_print_errors_fp(stderr);
+    goto cleanup;
+  }
+  if (EVP_DigestVerifyUpdate(ctx, message, message_len) != 1) {
+    fprintf(stderr, "Error: EVP_DigestVerifyUpdate failed\n");
+    ERR_print_errors_fp(stderr);
+    goto cleanup;
+  }
+  int rc = EVP_DigestVerifyFinal(ctx, signature, signature_len);
+  if (rc == 1) {
+    ret = 1;
+  } else if (rc == 0) {
+    ret = 0;
+  } else {
+    fprintf(stderr, "Error: EVP_DigestVerifyFinal failed\n");
+    ERR_print_errors_fp(stderr);
+    ret = -1;
+  }
+cleanup:
+  EVP_MD_CTX_free(ctx);
+  return ret;
+}
